31a94834cab8142ba38e4c15f8ef1f03b34914d4
Jonathan Pickett <jonathanpickett@me.com>
2016-08-17 16:49:09 -0700

    Create Table View

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jonny Pickett on 8/17/16.
+//  Copyright © 2016 Jonathan Pickett. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    // MARK: Initialization
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
